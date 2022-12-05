28e7c9da95b60a0f2057d08bd405cd47a852f3b5
Yahia Masud <79189930+yahiamasud@users.noreply.github.com>
2022-12-05 21:54:16 +0600

    Add files via upload

---
+++ accesst/js/mystyle.js
@@ -0,0 +1,11 @@
+$('.csta').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear'
+  });
+          
+          
